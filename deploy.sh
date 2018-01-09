5593b205e1cb5981593e8d9721aadd5ae735edf6
alexander.petrov <alexander.petrov@ALEXANDERPETR.int.nordigy.ru>
2018-01-09 11:55:47 +0300

    added deploy for windows

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 set -ex
 
 declare TOP=$(dirname "$0")
-declare FREEPLANE_DIR="$HOME/.config/freeplane/1.6.x"
+declare FREEPLANE_DIR=
+[[ ! -z $OS && $OS =~ Windows_NT ]] && FREEPLANE_DIR="$HOME/AppData/Roaming/Freeplane/1.6.x/" || FREEPLANE_DIR="$HOME/.config/freeplane/1.6.x"
 
 find $TOP/src/zips/lib/freeplaneGTD -name "*.groovy" | xargs -I{} cp {} $FREEPLANE_DIR/lib/freeplaneGTD
 find $TOP/src/scripts -name "*.groovy" | xargs -I{} cp {} $FREEPLANE_DIR/addons/freeplaneGTD/scripts
